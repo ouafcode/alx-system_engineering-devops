dda0a29e202d4b56fed3652aa2ce5c69a3c33a52
ouafcode <ouafaesaim@gmail.com>
2024-03-29 10:36:46 -0700

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("name")
+    username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as file:
