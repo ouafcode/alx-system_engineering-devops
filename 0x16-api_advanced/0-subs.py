f229f87bfe87196eabd51668c522fc4f90b35509
ouafcode <ouafaesaim@gmail.com>
2024-04-14 11:19:31 -0700

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """method doc"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Extra-Friendship-842/1.0"}
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
