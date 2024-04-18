885afbbfcc393541fae1c6c667550206924cea87
ouafcode <ouafaesaim@gmail.com>
2024-04-18 10:46:49 -0700

    task1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable the user to login and open file.
+
+# Increase limit file for user.
+exec { 'increase-hard-file':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit  user.
+exec { 'increase-soft-file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
